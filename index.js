9a5ff2cca6efc673a965ed6875c0497641972f00
jasonbai <15810683857@163.com>
2025-02-22 20:08:49 +0800

    微调

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
  *
  *   useful-cool-js.js - A Cool JavaScript Plugin Set
  *   Author: Jason Bai <byygkcg@163.com>
- *   Version: v13.0.0
- *   Url: https://github.com/jasonbai008/useful-cool-js
+ *   Github: https://github.com/jasonbai008/useful-cool-js
  *   License(s): MIT
  *
  */
